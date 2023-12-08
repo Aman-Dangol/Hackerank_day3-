c254f5b3b73661adc341460591992a05a764a81e
Aman Dangol <amandangol423@gmail.com>
2023-12-08 21:08:51 +0545

    delted file

--- day1/time_conversion.c
+++
@@ -1,8 +0,0 @@
-string timeConversion(string s) {
-    int pos = s.find(":");
- int t = stoi(s.substr(0,pos));
- string r = s.substr(8,2);
-if(t == 12 && r =="AM")return "00"+s.substr(pos,6);
-else if(t <12 && r=="PM")return to_string((t+12))+s.substr(pos,6);
-else return s.substr(0,8);
-}
