99386fd0489868182a78cd6eb69e3bccef2b8d91
Aman Dangol <amandangol423@gmail.com>
2024-01-02 11:54:20 +0545

    grid_challenge

--- day4/grid_challenge.js
+++ day4/grid_challenge.js
@@ -1,3 +1,23 @@
-function gridChallenge(){
-    
+function gridChallenge(grid) {
+  let sorted = new Array(); // to store the sorted array on a new variable
+  grid.forEach((element) => {
+    sorted.push(element.split("").sort()); //pushing each element into sorted after spliting them into characters and then sorting them
+  });
+  for (let i = 0; i < sorted.length; i++) {
+    for (let j = 0; j < sorted.length; j++) {
+      if (j + 1 >= sorted.length) {
+        break;
+      }
+      console.log(j);
+
+      if (sorted[j][i] > sorted[j + 1][i]) {
+        console.log("NO");
+        return "NO";
+      }
+    }
+  }
+  console.log("yes");
+  return "YES";
 }
+
+console.log(gridChallenge(["zzwj", "zjzw", "zwzj", "jzwz"]));
